c2c787e3c7c757c27dfe83e3b1a794d78f958a4b
Julien Vehent <julienv@aweber.com>
2012-10-28 01:06:30 -0400

    version bump \o/

--- metadata.rb
+++ metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "julien@linuxwall.info"
 license          "All rights reserved"
 description      "Installs/Configures the Advanced FireWall"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
