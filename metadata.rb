bc6d088db1d479ecc10fca4145c82c480bb93c56
Julien Vehent <julienv@aweber.com>
2013-04-10 15:47:08 -0400

    Release v0.0.7

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name             "afw"
 maintainer       "Julien Vehent"
 maintainer_email "julien@linuxwall.info"
 license          "All rights reserved"
 description      "Installs/Configures the Advanced FireWall"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.0.7"
